02daf1b058641e1b1e1fb93194c5adf311134589
rick <technoweenie@gmail.com>
2009-02-16 00:14:51 -0800

    strip dashes and parens from generated method names.  ensure that TestCoreExt actually tests something

--- lib/context/core_ext/string.rb
+++ lib/context/core_ext/string.rb
@@ -2,16 +2,24 @@ class String
   # Replaces spaces and tabs with _ so we can use the string as a method name
   # Also replace dangerous punctuation
   def to_method_name
-    self.downcase.gsub(/[\s:',;!#]+/,'_')
+    downcased = self.downcase
+    downcased.gsub!(/[\s:',;!#\-\(\)]+/,'_')
+    downcased
   end
   
   # Borrowed from +camelize+ in ActiveSupport
   def to_module_name
-    self.to_method_name.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    meth_name = self.to_method_name
+    meth_name.gsub!(/\/(.?)/) { "::#{$1.upcase}" }
+    meth_name.gsub!(/(?:^|_)(.)/) { $1.upcase }
+    meth_name
   end
   
   # Borrowed from +camelize+ in ActiveSupport
   def to_class_name
-    self.to_method_name.gsub(/\/(.?)/) { "#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    meth_name = self.to_method_name
+    meth_name.gsub!(/\/(.?)/) { "#{$1.upcase}" }
+    meth_name.gsub!(/(?:^|_)(.)/) { $1.upcase }
+    meth_name
   end
 end
